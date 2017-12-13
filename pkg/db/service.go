d045264cac18ea7e0cf8a346625f4462cfbfc191
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2017-12-13 17:37:16 +0100

    added concurrency service test

--- pkg/db/service.go
+++ pkg/db/service.go
@@ -4,12 +4,39 @@ import "github.com/gerlacdt/db-example/pb"
 
 // Service provides all database methods
 type Service struct {
-	db *Db
+	db                *Db
+	SetRequestChannel chan *SetRequest
+}
+
+// SetRequest is input for the worker non-buffered channel. Response will be
+// send to the resultChannel
+type SetRequest struct {
+	Entity        *pb.Entity
+	ResultChannel chan error
+}
+
+// NewService creates a new db-service based on the given db reference
+func NewService(db *Db) *Service {
+	c := make(chan *SetRequest)
+	service := &Service{db: db, SetRequestChannel: c}
+
+	// start single background thread for SET request in order to prevent
+	// concurrency issues during save.
+	go func(queryChan <-chan *SetRequest) {
+		for request := range queryChan {
+			err := service.db.Set(request.Entity)
+			request.ResultChannel <- err
+		}
+	}(c)
+
+	return service
 }
 
 // Set and stores given entity
 func (service *Service) Set(entity *pb.Entity) error {
-	err := service.db.Set(entity)
+	c := make(chan error)
+	service.SetRequestChannel <- &SetRequest{Entity: entity, ResultChannel: c}
+	err := <-c
 	if err != nil {
 		return err
 	}
