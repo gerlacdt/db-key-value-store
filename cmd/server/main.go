1ba21084bc2d2cb9fea81f50ce0161b9b94393d6
Daniel Gerlach <daniel.gerlach1981@googlemail.com>
2017-12-15 18:37:56 +0100

    added graceful shutdown for http server (handle SIGINT and SIGTERM)

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/gerlacdt/db-example/pkg/config"
 	"github.com/gerlacdt/db-example/pkg/db"
@@ -13,8 +17,31 @@ func main() {
 	config := config.NewConfig()
 	fmt.Println("start app...")
 	handler := db.NewMainHandler(config.Filename)
-	err := http.ListenAndServe(":"+config.Port, handler)
-	if err != nil {
-		log.Fatalf("error listening %v", err)
+
+	// graceful shutdown
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+	srv := &http.Server{
+		Addr:    ":" + config.Port,
+		Handler: handler,
 	}
+
+	go func() {
+		log.Fatal(srv.ListenAndServe())
+	}()
+	fmt.Println("App is ready to listen and serve.")
+
+	killSignal := <-interrupt
+	switch killSignal {
+	case os.Interrupt:
+		fmt.Println("Got SIGINT...")
+	case syscall.SIGTERM:
+		fmt.Println("got SIGTERM...")
+	}
+	fmt.Println("App is shutting down...")
+	err := srv.Shutdown(context.Background())
+	if err != nil {
+		fmt.Printf("Error shutting down: %v\n", err)
+	}
+	fmt.Println("Done")
 }
